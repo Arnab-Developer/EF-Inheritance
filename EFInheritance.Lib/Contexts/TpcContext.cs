f094eaf842a435f8947f6a2ff1368b436ee60cc2
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2024-05-14 12:12:18 +0530

    tpc setting

--- EFInheritance.Lib/Contexts/TpcContext.cs
+++ EFInheritance.Lib/Contexts/TpcContext.cs
@@ -10,4 +10,15 @@ public class TpcContext(DbContextOptions<TpcContext> options) : DbContext(option
     public DbSet<Cat> Cats { get; set; }
 
     public DbSet<Dog> Dogs { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.Entity<Animal>().UseTpcMappingStrategy().ToTable("Animals");
+        builder.Entity<Cat>().ToTable("Cats");
+        builder.Entity<Dog>().ToTable("Dogs");
+    }
 }
+
+// Run this to create migration
+// Add-Migration -Name "InitialCreate" -Context "EFInheritance.Lib.Contexts.TpcContext" -OutputDir "Migrations\Tpc"
+// Update-Database -Context "EFInheritance.Lib.Contexts.TpcContext"

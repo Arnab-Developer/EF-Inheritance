5bfddb6d6aa47e43294e0c900ea609e9d3582b36
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2024-05-14 11:59:42 +0530

    tpt setting in on model creation

--- EFInheritance.Lib/Contexts/TptContext.cs
+++ EFInheritance.Lib/Contexts/TptContext.cs
@@ -10,4 +10,13 @@ public class TptContext(DbContextOptions<TptContext> options) : DbContext(option
     public DbSet<Cat> Cats { get; set; }
 
     public DbSet<Dog> Dogs { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.Entity<Animal>().UseTptMappingStrategy();
+    }
 }
+
+// Run this to create migration
+// Add-Migration -Name "InitialCreate" -Context "EFInheritance.Lib.Contexts.TptContext" -OutputDir "Migrations\Tpt"
+// Update-Database -Context "EFInheritance.Lib.Contexts.TptContext"
